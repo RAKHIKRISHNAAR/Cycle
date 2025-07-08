c4f5e124607d97ec26153c3cdb156c4e4b084dba
RAKHIKRISHNAAR <rakhykrishnaa5ar@gmail.com>
2025-07-08 22:56:08 +0530

    frontend url render

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,26 +1,52 @@
 
-
-
-
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Container, Form, Row, Col } from 'react-bootstrap';
+import axios from "axios"
 
 function Contact() {
+
+  const[formData ,setFormData]=useState({
+    name:"",
+    email:"",
+    phone:"",
+    place:"",
+    message:""
+  })
+
+  const handleChange=(e)=>{
+    setFormData({...formData,[e.target.name]:e.target.value})
+  };
+
+  const handleSubmit=async(e)=>{
+    e.preventDefault()
+    try{
+      // const res=await axios.post("http://localhost:8000/api/contact",formData)
+            const res=await axios.post("https://cycle-5klv.onrender.com/api/contact",formData)
+
+      alert(res.data.message)
+      setFormData({name:"",email:"",phone:"",place:"",message:""})
+    }catch(error){
+      alert("something went wrong")
+    }
+  }
   return (
     <Container className="py-5">
       <div className="p-4 rounded-3 shadow" style={{ background: 'white', maxWidth: '800px', margin: '0 auto' }}>
         <h2 className="text-center mb-4" style={{ color: '#2c3e50' }}>Contact Us</h2>
         
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Row className="mb-3">
             <Col md={6} className="mb-3 mb-md-0">
               <Form.Group controlId="formName">
                 <Form.Label>Name</Form.Label>
                 <Form.Control 
+                name="name"
                   type="text" 
                   placeholder="Enter your name" 
                   required 
                   className="py-2"
+                  value={formData.name}
+                  onChange={handleChange}
                 />
               </Form.Group>
             </Col>
@@ -28,10 +54,14 @@ function Contact() {
               <Form.Group controlId="formEmail">
                 <Form.Label>Email</Form.Label>
                 <Form.Control 
+                name="email"
                   type="email" 
                   placeholder="Enter your email" 
                   required 
                   className="py-2"
+                  value={formData.email}
+                  onChange={handleChange}
+
                 />
               </Form.Group>
             </Col>
@@ -41,10 +71,14 @@ function Contact() {
             <Col md={6} className="mb-3 mb-md-0">
               <Form.Group controlId="formPhone">
                 <Form.Label>Phone Number</Form.Label>
-                <Form.Control 
+                <Form.Control
+                name="phone" 
                   type="tel" 
                   placeholder="Enter your phone number" 
                   className="py-2"
+                  value={formData.phone}
+                   onChange={handleChange}
+
                 />
               </Form.Group>
             </Col>
@@ -52,9 +86,13 @@ function Contact() {
               <Form.Group controlId="formPlace">
                 <Form.Label>Place</Form.Label>
                 <Form.Control 
+                name="place"
                   type="text" 
                   placeholder="Enter your location" 
                   className="py-2"
+                  value={formData.place}
+                 onChange={handleChange}
+
                 />
               </Form.Group>
             </Col>
@@ -64,10 +102,14 @@ function Contact() {
             <Form.Label>Message</Form.Label>
             <Form.Control 
               as="textarea" 
+              name="message"
               rows={4} 
               placeholder="Your message here..." 
               required 
               className="py-2"
+              value={formData.message}
+              onChange={handleChange}
+
             />
           </Form.Group>
 
